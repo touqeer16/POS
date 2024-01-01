4270c3f74bc57ad5b579d1cf2e89942cad0a1f8b
touqeer16 <touqeerfazal1992@gmail.com>
2024-01-01 10:29:34 +0300

    new changes

--- Modules/Woocommerce/Config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'name' => 'Woocommerce',
-    'module_version' => "2.6"
-];
--- resources/pos/src/environment.js
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    // URL: window.location.protocol + '//' + window.location.hostname,
-    //URL: 'https://tek-store.net',
-    URL: window.location.protocol + '//' + window.location.hostname + ":8000",
-    //URL: 'https://fastich.com',
-    //URL: 'https://stage.fastich.com',
-
-}
